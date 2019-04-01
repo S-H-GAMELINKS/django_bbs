5d1028967f098a1580a8645507dccf081355b637
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-01 13:20:59 +0900

    add Topics App for INSTALLED_APP

--- django_bbs/settings.py
+++ django_bbs/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'topics.apps.TopicsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
