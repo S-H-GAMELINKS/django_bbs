79dd93b76a1399b4bc0f3626360f836bcf7a666e
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-01 13:22:46 +0900

    fix model

--- topics/models.py
+++ topics/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Topic(models.Model):
-    title_text = models.CharField((max_length=30))
+    title_text = models.CharField(max_length=30)
     content_text = models.CharField(max_length=140)
-    
+
