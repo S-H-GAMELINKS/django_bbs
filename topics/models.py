14a2f60b91872f41ca7fae57800363c07014cae8
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-04-01 15:19:31 +0900

    add Comment model

--- topics/models.py
+++ topics/models.py
@@ -4,3 +4,6 @@ class Topic(models.Model):
     title_text = models.CharField(max_length=30)
     content_text = models.CharField(max_length=140)
 
+class Comment(models.Model):
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE, default='')
+    content_text = models.CharField(max_length=140)
